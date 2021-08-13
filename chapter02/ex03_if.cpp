4e7cd54abeeab46826c6ad736c406d3c3c7c2fba
chuckpark <eirc0593@gmail.com>
2021-08-13 17:53:11 +0900

    chapter02

--- chapter02/ex03_if.cpp
+++ chapter02/ex03_if.cpp
@@ -4,9 +4,12 @@ using namespace std;
 int main(int argc, char const *argv[]) {
     int x =100;
 
-    if(x == 100) {
+    // 한 줄이면 중괄호 생략가능 (있어도 상관없음)
+    if(x == 100) 
         cout << "x가 100입니다." << endl;
-  
+    
+    //  두 줄부턴 반드시 중괄호 있어야함
+    if(x == 100) {
         cout << "x의 값을 출력합니다." << endl;
         cout << "x가 100입니다." << endl;
     }
